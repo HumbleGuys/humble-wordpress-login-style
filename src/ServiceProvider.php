1a1cda5b8338e45973c3920ff94eea53458a325b
Kim Larsson <kim@humbleguys.se>
2022-08-29 19:26:09 +0200

    Removed filter for svg support

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,7 +4,6 @@ namespace HumbleWordPressLoginStyle;
 
 use HumbleCore\Support\Facades\Action;
 use HumbleCore\Support\Facades\Filter;
-use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider as SupportServiceProvider;
 
 class ServiceProvider extends SupportServiceProvider
@@ -23,10 +22,6 @@ class ServiceProvider extends SupportServiceProvider
         ]);
 
         app('acf.fields')->registerFieldGroup(ACFFields::class);
-
-        Filter::add('upload_mimes', function ($mimes) {
-            return Arr::add($mimes, 'svg', 'image/svg+xml');
-        });
 
         Action::add('login_enqueue_scripts', LoginStyle::class);
 
